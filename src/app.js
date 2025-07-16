09eb6896205f66115e294889a27486889c9f6e48
tariqul10765 <tariqul10765@gmail.com>
2025-07-16 16:07:25 +0600

    Generate Project boilarplate & PostgreSQL connection

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const prisma = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Sample route
+app.get("/", async (req, res) => {
+    // Find user by email
+    const user = await prisma.user.findUnique({ where: { email: "test@test.com" } });
+    res.send('API is running 🚀', user);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
